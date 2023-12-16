1f84ab42f4a6affbe85d4d9c209c9b706355e524
Pedro Santos <anda-cun@student.42lisboa.com>
2023-12-16 08:13:06 +0000

    m

--- ex01/Fixed.h
+++ ex01/Fixed.h
@@ -6,12 +6,12 @@
 /*   By: anda-cun <anda-cun@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 22:05:46 by pedrosantos       #+#    #+#             */
-/*   Updated: 2023/12/09 14:45:01 by anda-cun         ###   ########.fr       */
+/*   Updated: 2023/12/11 22:43:22 by anda-cun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __FIXED_HPP__
-#define __FIXED_HPP__
+#ifndef __FIXED_H__
+#define __FIXED_H__
 
 #include "Fixed.hpp"
 #include <iostream>
--- ex02/Fixed.h
+++ ex02/Fixed.h
@@ -6,12 +6,12 @@
 /*   By: anda-cun <anda-cun@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 22:05:46 by pedrosantos       #+#    #+#             */
-/*   Updated: 2023/12/09 14:44:53 by anda-cun         ###   ########.fr       */
+/*   Updated: 2023/12/11 22:43:29 by anda-cun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __FIXED_HPP__
-#define __FIXED_HPP__
+#ifndef __FIXED_H__
+#define __FIXED_H__
 
 #include "Fixed.hpp"
 #include <iostream>
