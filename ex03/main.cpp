2611bcc5661c4aef7c8bdf432b80b7e6bd9ef0fb
Antonio Pedro Da Cunha Santos <anda-cun@c2r4s7.42lisboa.com>
2024-02-03 10:12:57 +0000

    add README

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -27,20 +27,11 @@ int main(void)
     Point C(7, 5);
     Point P(5.9f, 3.7f);
     
-    if (bsp(A, B, C, P))
-        std::cout << "Point is inside triangle\n";
-    else
-        std::cout << "Point is outside triangle\n";
-    
+
+    bsp(A, B, C, P) ? std::cout << "Point is inside triangle\n" : std::cout << "Point is outside triangle\n";
     Point P2(6.25f, 3.75f);
-    if (bsp(A, B, C, P2))
-        std::cout << "Point is inside triangle\n";
-    else
-        std::cout << "Point is outside triangle\n";
+    bsp(A, B, C, P2) ? std::cout << "Point is inside triangle\n" : std::cout << "Point is outside triangle\n";
     Point P3(5, 2);
-    if (bsp(A, B, C, P3))
-        std::cout << "Point is inside triangle\n";
-    else
-        std::cout << "Point is outside triangle\n";
+    bsp(A, B, C, P3) ? std::cout << "Point is inside triangle\n" : std::cout << "Point is outside triangle\n";
     return 0;
 }
